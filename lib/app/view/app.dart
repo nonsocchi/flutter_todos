41b55e659cda74225015c11a0e8bcd3d1ae8c1f8
nonsocchi <nonsocchi@gmail.com>
2023-06-25 21:40:22 +0100

    init

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todos/counter/counter.dart';
-import 'package:flutter_todos/l10n/l10n.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_todos/l10n/l10n.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
